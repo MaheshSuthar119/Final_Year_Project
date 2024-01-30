116dcc68045890ffcc8ae8086a1e5a3ced14bf86
pravven <pravven16@gmail.com>
2024-01-30 21:05:05 +0530

    backend added

---
+++ Ecommerce-frontend/src/pages/productDetailPage.js
@@ -0,0 +1,11 @@
+import NavBar from "../features/navbar/Navbar";
+import ProductDetail from "../features/product/componnets/ProductDetail";
+function ProductDetailPage() {
+    return ( <div>
+        <NavBar>
+            <ProductDetail></ProductDetail>
+        </NavBar>
+    </div> );
+}
+
+export default ProductDetailPage;
